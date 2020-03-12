ea3defa0bfc9539de72cf6e91b5bfdabc839fd5e
Zheng, Lei <realthunder.dev@gmail.com>
2020-03-12 12:15:20 +0800

    Gui: fix ExpressionCompleterModel::Level1Data

--- src/Gui/ExpressionCompleter.cpp
+++ src/Gui/ExpressionCompleter.cpp
@@ -865,7 +865,7 @@ public:
         }
 
         virtual QModelIndex childIndex(int row) {
-            if(getModel()->searchingUnit || row >= childCountWithoutUnit())
+            if(getModel()->searchingUnit || mindex.row() >= childCountWithoutUnit())
                 return QModelIndex();
 
             App::Document *doc = nullptr;
