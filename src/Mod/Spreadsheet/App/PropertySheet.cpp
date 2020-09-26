2b06ec562ea403214f230abb505c52c0b9d51ec4
Zheng, Lei <realthunder.dev@gmail.com>
2020-09-26 04:04:22 +0000

    App/Spreadsheet: fix Spreadsheet alias renaming and moving

--- src/Mod/Spreadsheet/App/PropertySheet.cpp
+++ src/Mod/Spreadsheet/App/PropertySheet.cpp
@@ -34,6 +34,7 @@
 #include <App/Document.h>
 #include <App/DocumentObject.h>
 #include <App/Property.h>
+#include <App/Application.h>
 #include <Base/Writer.h>
 #include <Base/Reader.h>
 #include <Base/Tools.h>
@@ -651,9 +652,12 @@ void PropertySheet::setAlias(CellAddress address, const std::string &alias)
         App::ObjectIdentifier key(owner, oldAlias);
         App::ObjectIdentifier value(owner, alias.empty()?address.toString():alias);
 
-        m[key] = value;
+        m[key.canonicalPath()] = value;
 
-        owner->getDocument()->renameObjectIdentifiers(m);
+        for (auto doc : App::GetApplication().getDocuments()) {
+            for (auto obj : doc->getObjects())
+                obj->renameObjectIdentifiers(m);
+        }
     }
 
     signaller.tryInvoke();
@@ -741,10 +745,6 @@ void PropertySheet::moveCell(CellAddress currPos, CellAddress newPos, std::map<A
 
         // Remove merged cell data
         splitCell(currPos);
-
-        std::string alias;
-        if(cell->getAlias(alias))
-            cell->setAlias("");
 
         // Remove from old
         removeDependencies(currPos);
